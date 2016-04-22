bc9a677fca98b808ed443f681b0d543080defaba
Maryan Mandziuk <maryan.mandziuk@gmail.com>
2016-04-22 23:15:55 +0300

    Integrating commons-cli

--- src/java/net/taunova/template/Application.java
+++ src/java/net/taunova/template/Application.java
@@ -9,7 +9,13 @@ package net.taunova.template;
 import net.taunova.template.file.FilesystemWalker;
 import java.io.File;
 import java.io.IOException;
-
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+//import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 /**
  * Template application entry point and arguments processing.
  *
@@ -20,39 +26,72 @@ public class Application {
     private static final String MAIN_PROPERTIES = "main";
     
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: <in-folder> <out-folder>");
-            System.exit(1);
-        }
-
-        final File inFolder = new File(args[0]);
-        final File outFolder = new File(args[1]);
-
-        boolean exit = false;
-
-        if (!inFolder.isDirectory()) {
-            System.err.println("Input folder does not exist: "
-                    + inFolder.getAbsolutePath());
-            exit = true;
-        }
-
-        if (!outFolder.isDirectory()) {
-            System.err.println("Output folder does not exist: "
-                    + outFolder.getAbsolutePath());
-            exit = true;
-        }
-
-        if (exit) {
-            System.out.println("terminating...");
-            System.exit(1);
-        }               
         
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+        
+
+//        options.addOption("h", "help", false, "Show this help");
+        
+        options.addOption(Option.builder("f")
+                .numberOfArgs(2)
+                .required()
+                .desc("requirie two folders")
+                .build());
+
         try {
-            final String settingsName = (args.length > 2) ? args[2] : MAIN_PROPERTIES;
-            FilesystemWalker app = new FilesystemWalker(settingsName);
-            app.processFolder(inFolder, outFolder.getAbsolutePath(), false);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            CommandLine commandLine = parser.parse(options, args);
+            
+            if(commandLine.hasOption("f")) {
+                
+                final File inFolder = new File(commandLine.getOptionValues("f")[0]);
+                final File outFolder = new File(commandLine.getOptionValues("f")[1]);
+                
+                        
+
+                if (!inFolder.isDirectory()) {
+                    System.err.println("Input folder does not exist: "
+                            + inFolder.getAbsolutePath());
+                   
+                }
+
+                if (!outFolder.isDirectory()) {
+                    System.err.println("Output folder does not exist: "
+                            + outFolder.getAbsolutePath());
+                    
+                }
+
+                    try {
+                        final String settingsName = (args.length > 2) ? args[2] : MAIN_PROPERTIES;
+                        FilesystemWalker app = new FilesystemWalker(settingsName);
+                        app.processFolder(inFolder, outFolder.getAbsolutePath(), false);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                
+                
+            }
+            
+            
+//            HelpFormatter formatter = new HelpFormatter();
+//            if(commandLine.hasOption("h")) {
+//                
+//                
+//                formatter.printHelp("CommandLineParameters", options);
+//            } else if(commandLine.getOptions().length == 0) {
+//                formatter.printHelp("CommandLineParameters", options);
+//            }
+            
+            
+        } catch(ParseException e) {
+            e.printStackTrace();
         }
+
+//        if (exit) {
+//            System.out.println("terminating...");
+//            System.exit(1);
+//        }               
+        
+        
     }
 }
